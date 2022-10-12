df3063b0acf1570cd0579309f10304a08e07d4eb
Cesar Augusto <cesinhalius@hotmail.com>
2022-10-12 18:30:17 -0300

    first commit

---
+++ Sobrecarga/ExerciciosPratico2/src/Exercicios/Main.java
@@ -0,0 +1,11 @@
+package Exercicios;
+
+public class Main {
+    public static void main(String[] args) {
+        //Quadrilatero
+        System.out.println("Exercicio");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5,5);
+        Quadrilatero.area(7,8,9);
+    }
+}
